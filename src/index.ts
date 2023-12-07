227502c3645ffc72d60f4f877c5293b6a62f1295
NicolasGorga <nicogorga11@gmail.com>
2023-12-07 14:56:48 -0300

    third test, after adding the husky pre-commit hook to .husky folder

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ const add = (a: number, b: number) => {
     return a + b;
 };
 
-const number1 = 3
+const number1 = 3;
 const number2 = 2;
-const result = number1 + number2;
-
-var test = '';
+const result = add(number1, number2);
 
 console.log(`Result is: ${result}`);
