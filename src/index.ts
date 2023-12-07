bb4ca837a43e3c37f73d001b664612f09141e494
NicolasGorga <nicogorga11@gmail.com>
2023-12-07 14:46:09 -0300

    Test lint-staged y husky

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ const add = (a: number, b: number) => {
     return a + b;
 };
 
-const number1 = 3
-const number2 = 2;
+const number1 = 3;
+const number2 = 2
 const result = number1 + number2;
 
 var test = '';
